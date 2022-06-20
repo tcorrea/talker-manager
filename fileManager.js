5efa04f27eb927317c98355d44b616d5dba0968a
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-06-20 17:29:14 -0300

    testando requisito 1.1

--- fileManager.js
+++ fileManager.js
@@ -1,16 +1,13 @@
 const fs = require('fs/promises');
 
+const readContentFile = async () => {
+  return JSON.parse(await fs.readFile('./talker.json'));
+};
+
 const writeContentFile = async (data) => {
   const talkers = await readContentFile();
   talkers.push(data);
   await fs.writeFile('./talker.json', JSON.stringify(talkers));
-}
+};
 
-const readContentFile = async () => {
-  return JSON.parse(await fs.readFile('./talker.json'));
-}
-
-module.exports = {
-  writeContentFile,
-  readContentFile,
-} 
+module.exports = { writeContentFile, readContentFile };
