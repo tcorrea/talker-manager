952ce5ab3811f224895b342e022e4713ac777a4d
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-06-22 16:53:53 -0300

    fix: lint

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -95,17 +95,14 @@ const isValidWatchedAt = (req, res, next) => {
 const isValidRate = (req, res, next) => {
   const { talk } = req.body;
   const { rate } = talk;
-
-  if (!rate && rate != 0) {
+  if (!rate && Number(rate) !== 0) {
     return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   }
-
   if (!Number.isInteger(rate) || Number(rate) <= 0 || Number(rate) > 5) {
     return res
       .status(400)
       .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
-
   next();
 };
 
